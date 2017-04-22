038103028c84c5ef50e0d6d5b09cd099e24d493b
kadomts <kadomts@gmail.com>
2017-04-22 21:46:59 +0400

    add import sql libs

--- src/main/java/com/kadomts/util/DBUtil.java
+++ src/main/java/com/kadomts/util/DBUtil.java
@@ -1,4 +1,11 @@
 package com.kadomts.util;
 
+import java.sql.*;
+
 public class DBUtil {
+  public static Connection connection = null;
+
+  public static Connection getConnection(){
+      return null;
+  }
 }
