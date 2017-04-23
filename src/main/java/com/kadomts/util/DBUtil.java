1e8eb65f75de2a7b8f8ecf429a2c3775a4aa7a04
kadomts <kadomts@gmail.com>
2017-04-23 15:04:48 +0400

    Update DBUtil class

--- src/main/java/com/kadomts/util/DBUtil.java
+++ src/main/java/com/kadomts/util/DBUtil.java
@@ -1,11 +1,35 @@
 package com.kadomts.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class DBUtil {
-  public static Connection connection = null;
+  private static Connection connection = null;
 
   public static Connection getConnection(){
-      return null;
+      if (connection != null){
+          return connection;
+      }else {
+          try {
+              Properties properties = new Properties();
+              InputStream inputStream = DBUtil.class.getResourceAsStream("db.properties");
+              properties.load(inputStream);
+              String driver = properties.getProperty("driver");
+              String url = properties.getProperty("url");
+              String user = properties.getProperty("user");
+              String password = properties.getProperty("password");
+              Class.forName(driver);
+              connection = DriverManager.getConnection(url, user, password);
+          }catch (IOException e){
+              e.printStackTrace();
+          }catch (ClassNotFoundException e){
+              e.printStackTrace();
+          }catch (SQLException e){
+              e.printStackTrace();
+          }
+          return connection;
+      }
   }
 }
